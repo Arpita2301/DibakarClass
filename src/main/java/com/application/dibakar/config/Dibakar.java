f1e22224a677fb5ef6a462f8343a718586be21e6
Arpita2301 <arpita23mallik@gmail.com>
2022-08-26 00:48:40 +0530

    first commit

---
+++ src/main/java/com/application/dibakar/config/Dibakar.java
@@ -0,0 +1,34 @@
+package com.application.dibakar.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.TimeZone;
+
+@Configuration
+public class Dibakar {
+
+    @Value("${timeZone}")
+    public String timeZone;
+
+    @Autowired
+    public void configureJackson(ObjectMapper objectMapper) {
+        objectMapper.setTimeZone(TimeZone.getTimeZone(timeZone));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
+
